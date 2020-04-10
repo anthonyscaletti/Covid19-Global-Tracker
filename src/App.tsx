ed33698bf001b7306af048389636474643a86343
Tony Scaletti <ascaletti@shoplogix.com>
2020-04-10 13:32:13 -0400

    Created Skeleton React Project Structure CGT-1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/main.css';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Covid19 Global Tracker</h1>
+			</React.Fragment>
+		);
+	}
+}
+
+export default App;
