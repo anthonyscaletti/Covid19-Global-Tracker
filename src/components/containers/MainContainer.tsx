4c114a0d26badf67b5c289bf20152b61407ae878
Tony Scaletti <ascaletti@shoplogix.com>
2020-04-10 16:46:55 -0400

    Changed CSS

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -5,9 +5,9 @@ import '../../styles/main.css';
 class MainContainer extends React.Component {
     render() {
         return (
-            <React.Fragment>
+            <div className="mainContainer">
                 <h1>Main Container</h1>
-            </React.Fragment>
+            </div>
         );
     }
 }
